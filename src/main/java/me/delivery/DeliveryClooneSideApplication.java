47e76b4377c039ae2c6282eef33701775e97bdec
TaeWonn <xodns9485@gmail.com>
2022-07-17 16:23:16 +0900

    First Commit

---
+++ src/main/java/me/delivery/DeliveryClooneSideApplication.java
@@ -0,0 +1,13 @@
+package me.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryClooneSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryClooneSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/delivery/DeliveryClooneSideApplicationTests.java
@@ -0,0 +1,13 @@
+package me.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryClooneSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
